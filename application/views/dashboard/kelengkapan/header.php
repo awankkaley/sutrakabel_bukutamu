2e665356b633936fe8d0e2d498eb926f24b529f8
Awank Kaley <38899883+awankkaley@users.noreply.github.com>
2020-12-16 06:39:30 +0700

    ok

--- application/views/dashboard/kelengkapan/header.php
+++ application/views/dashboard/kelengkapan/header.php
@@ -47,7 +47,7 @@
             <!-- Navigation -->
             <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
                 <div class="navbar-header">
-                    <a class="navbar-brand" href="index.html">PT. SUTRAKABEL</a>
+                    <a class="navbar-brand" href="<?php echo base_url() ?>">PT. SUTRAKABEL</a>
                 </div>
 
                 <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
@@ -57,9 +57,12 @@
                     <span class="icon-bar"></span>
                 </button>
 
-                <!-- <ul class="nav navbar-nav navbar-left navbar-top-links">
-                    <li><a href="#"><i class="fa fa-home fa-fw"></i> Website</a></li>
-                </ul> -->
+                <ul class="nav navbar-nav navbar-left navbar-top-links">
+                <li><a href="<?php echo base_url('dashboard') ?>" class="active"><i class="fa fa-dashboard fa-fw"></i> Dashboard</a></li>
+                <li><a href="<?php echo base_url('databukutamu') ?>"><i class="fa fa-table fa-fw"></i> Buku Tamu</a></li>
+                <li><a href="<?php echo base_url('datagrafik') ?>"><i class="fa fa-table fa-pie-chart"></i> Grafik Pengunjung</a></li>
+
+                </ul>
 
                 <ul class="nav navbar-right navbar-top-links">
                     <!-- <li class="dropdown navbar-inverse">
@@ -127,13 +130,5 @@
                 </ul>
                 <!-- /.navbar-top-links -->
 
-                <div class="navbar-default sidebar" role="navigation">
-                    <div class="sidebar-nav navbar-collapse">
-                        <ul class="nav" id="side-menu">
-                            <li><a href="<?php echo base_url('dashboard') ?>" class="active"><i class="fa fa-dashboard fa-fw"></i> Dashboard</a></li>
-                            <li><a href="<?php echo base_url('databukutamu') ?>"><i class="fa fa-table fa-fw"></i> Buku Tamu</a></li>
-                            <li><a href="<?php echo base_url('datagrafik') ?>"><i class="fa fa-table fa-pie-chart"></i> Grafik Pengunjung</a></li>
-                        </ul>
-                    </div>
-                </div>
+
             </nav>
